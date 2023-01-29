df8ae974d72676de64ad893291417074f024e040
Mayemba Bamba <mayemba_bamba@outlook.com>
2023-01-29 18:20:17 -0500

    add host ip address in main file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvi_config = uvicorn.Config("server:app", port=5000, log_level="info")
+    uvi_config = uvicorn.Config("server:app", host="0.0.0.0", port=5000, log_level="info")
     server = uvicorn.Server(uvi_config)
     server.run()
